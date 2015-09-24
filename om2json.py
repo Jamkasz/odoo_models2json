b7c2d73e4526e961bf84a2d78b059675d1b6713f
joelortiz <joelortiz@neovahealth.co.uk>
2015-09-24 17:49:26 +0100

    Add relations and models related to those relations

--- om2json.py
+++ om2json.py
@@ -26,8 +26,22 @@ class OdooField:
             raise TypeError('String expected, received %s' % type(field_name))
         if not isinstance(field_type, str):
             raise TypeError('String expected, received %s' % type(field_type))
-        self.n = field_name
-        self.t = field_type
+        self.name = field_name
+        self.type = field_type
+
+
+class OdooRelation:
+
+    def __init__(self, name, multiplicity, model):
+        if not isinstance(name, str):
+            raise TypeError('String expected, received %s' % type(name))
+        if not isinstance(multiplicity, str):
+            raise TypeError('String expected, received %s' % type(multiplicity))
+        if not isinstance(model, str):
+            raise TypeError('String expected, received %s' % type(model))
+        self.name = name
+        self.type = multiplicity
+        self.model = model
 
 
 class OdooClass:
@@ -35,14 +49,19 @@ class OdooClass:
     def __init__(self, name):
         if not isinstance(name, str):
             raise TypeError('String expected, received %s' % type(name))
-        self.n = name
+        self.name = name
         self.fields = []
         self.relations = []
 
     def add_field(self, field):
         if not isinstance(field, OdooField):
-            raise TypeError('Field expected, received %s' % type(field))
+            raise TypeError('OdooField expected, received %s' % type(field))
         self.fields.append(field)
+
+    def add_relation(self, relation):
+        if not isinstance(relation, OdooRelation):
+            raise TypeError('OdooRelation expected, received %s' % type(relation))
+        self.relations.append(relation)
 
 
 class CustomEncoder(JSONEncoder):
@@ -58,6 +77,7 @@ if args.u:
 
 client = erppeek.Client(SERVER_URL, DB_NAME, USERNAME)
 classes = []
+relation_models = []
 
 print 'Obtaining model list...'
 models = client.models()
@@ -65,12 +85,23 @@ models = {models[key]._name: models[key] for key in models.keys()}
 
 if args.f:
     models = {k: models[k] for k in models.keys() if args.f in k}
-print 'Model list obtained.'
 
 for model in models.keys():
     oclass = OdooClass(model)
     fields = models[model].fields()
-    [oclass.add_field(OdooField(f, fields[f]['type'])) for f in fields.keys()]
+    for f in fields.keys():
+        field_type = fields[f]['type']
+        if field_type == 'many2one' or field_type == 'many2many' or field_type == 'one2many':
+            relation_model = fields[f]['relation']
+            oclass.add_relation(OdooRelation(f, field_type, relation_model))
+            if relation_model not in relation_models and relation_model not in models.keys():
+                relation_models.append(relation_model)
+        else:
+            oclass.add_field(OdooField(f, field_type))
+    classes.append(oclass)
+
+for model in relation_models:
+    oclass = OdooClass(model)
     classes.append(oclass)
 
 print json.dumps(classes, cls=CustomEncoder)
